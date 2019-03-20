d35a7b1d6598c37bd94ccaa88d3f6f755fb1944d
TTT2866 <41979926+TTT2866@users.noreply.github.com>
2019-03-21 10:11:39 +1100

    Update Git updater.bat

--- Git updater.bat
+++ Git updater.bat
@@ -11,5 +11,13 @@ git status
 timeout /t 10
 git add global.bat 
 git commit -m "Commit %linenumber% global.bat"
+
+::I call my file global.bat for another project, you will have to change this to suit your program. Have it as any of these
+::git add . 
+::git add --all
+::git add -A
+
+::git commit -m "Commit %linenumber% file.whatever"
+
 git push -u origin master
 goto start
